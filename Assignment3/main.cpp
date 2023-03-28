6bd68100898ab3cfe6c160dec9c8c1df53854dc6
Sean Madzelonka <smadzelonka@gmail.com>
2023-03-27 19:08:08 -0500

    Initial Commit

---
+++ Assignment3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment3
+//
+//  Created by Sean Madzelonka on 3/27/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
